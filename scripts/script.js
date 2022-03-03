ee128af68c0dfefe78b3242fe215aca2983b6138
tarunfy <tarunsharma8920@gmail.com>
2022-03-03 19:18:25 +0530

    added dropdown functionality in ranking collection section

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,27 @@
+// Getting dom elements:
+const durationText = document.querySelector(".duration-text");
+const durationList = document.querySelector(".duration-list");
+const items = document.querySelectorAll(".item");
+
+// Adding event listners:
+durationText.addEventListener("click", (e) => {
+  durationList.classList.toggle("active");
+});
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    durationText.innerText = item.innerText;
+    durationList.classList.remove("active");
+  });
+});
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.querySelector("nav").style.top = "0";
+    document.querySelector("nav").style.top = "0px";
   } else {
-    document.querySelector("nav").style.top = "-70px";
+    document.querySelector("nav").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
 };
